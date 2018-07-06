034a39311a434984558dc9c48c53ba9998eb82bc
jalakpatoliya <30546867+jalakpatoliya@users.noreply.github.com>
2018-07-06 13:41:36 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ var express               = require("express");
     LocalStrategy         = require("passport-local"),
     passportLocalMongoose = require("passport-local-mongoose"),
     mongoose              = require("mongoose"),
-    User                  = require("./models/user"),
     expressSession        = require("express-session");
 
+    mongoose.connect("mongodb://localhost/auth_demo",{
+      useMongoClient:true
+    })
+    mongoose.Promise = global.Promise;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
-
+var User                  = require("./models/user"),
+    userRoute             = require('./routes/userRoute');
+//=========================================================
+//=============Initializing Session & Passport=============
+//=========================================================
 app.use(expressSession({
   secret:"Rusty is the best dog in the world", // is used to enco-deco information in the session
   resave:false,
@@ -19,83 +26,50 @@ app.use(expressSession({
 }))
 app.use(passport.initialize()); // always needed to use the passport methods
 app.use(passport.session());
+//=========================================================
+//=============User routes=======================
+//=========================================================
+app.use(userRoute);
 
-
-
-
-mongoose.connect("mongodb://localhost/auth_demo",{
-  useMongoClient:true
-})
-mongoose.Promise = global.Promise;
-
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser()); // it reades, decodes information in session,encodes it
-passport.deserializeUser(User.deserializeUser());
-//============
-// ROUTES
-//============
-
+//=========================================================
+//=============Normal Routes===============================
+//=========================================================
 app.get("/",function(req,res){
   res.render("home.ejs");
 })
 
 app.get("/secret",isLoggedIn,function(req,res){ //
+  console.log("user is logged in so,secret page is accessed");
   res.render("secret.ejs");
 })
-
-// Authorization ROUTES
-
-// Show sign up form
-app.get("/register",function(req,res){
-  res.render("register.ejs");
-})
-
-// handling user sign up
-app.post("/register",function(req,res){
-    User.register(new User({username: req.body.username}),req.body.password,function(err,user){
-      if (err) { // password is hashed by resiter and then saved in DB
-        res.send(err);
-        res.redirect("/register");
-      } else {
-        passport.authenticate("local")(req,res,function(){ // local can be replaced by twitter or fb
-          res.redirect("/secret");      //but here we are authenticating locally
-        })
-      }
-    })
-})
-
-// LOGIN ROUTES
-// Render login form
-app.get("/login",function(req,res){
-  res.render("login.ejs")
-})
-
-//login logic
-//MIDDLEWARE
-app.post("/login",passport.authenticate("local",{
-  successRedirect:"/secret",
-  failureRedirect:"/login"
-}),function(req,res){});
-
-//logout route
+//=========================================================
+//=============Authenticating function=====================
+//=========================================================
+//authenticating if user is loggedin or not
+function isLoggedIn(req,res,next) {
+  if (req.isAuthenticated()){
+    console.log("user is logged in");
+    return next();
+  }
+  console.log("user is not logged in");
+    res.redirect("/login");
+}
+//=========================================================
+//=============Logout route================================
+//=========================================================
 app.get("/logout",function(req,res){
   req.logout();
   res.redirect("/");
 })
+//=========================================================
+//============= To create Admin ===========================
+//=========================================================
 
-//authenticating if user is loggedin or not
-function isLoggedIn(req,res,next) {
-  if (req.isAuthenticated()){
-    console.log("is authenticated is true");
-    return next();
-  }
+User.register(new User({username:"jalak",role:"admin"}),"pass",function(err,user){})
 
-  console.log("is authenticated is false");
-    res.redirect("/login");
-
-
-}
-
+//=========================================================
+//=============Listening to port===========================
+//=========================================================
 app.listen(3823,function(){
   console.log("server connected");
 })
--- models/user.js
+++ models/user.js
@@ -1,9 +1,14 @@
-var monoose               = require("mongoose"),
+var mongoose               = require("mongoose"),
     passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
   username : String,
-  password : String
+  password : String,
+  role: {
+        type: String,
+        default: "user",
+        enum: ["user", "admin"]
+    }
 })
 
 UserSchema.plugin(passportLocalMongoose); // plugin connects all the passportLocalMongoose mehtods to UserSchema
